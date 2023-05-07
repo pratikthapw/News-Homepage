f62f7f945f297d259c0170f82a55fff44d0ed6a1
Pratik Thapa <pokhrelicreator@gmail.com>
2023-05-07 08:24:40 +0545

    Site Redesign With CSS Grid & FlexBox

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const nav__menu = nav.querySelector(".nav__menu");
 const hammenu = document.querySelector(".hammenu");
 const hammenu__close = hammenu.querySelector(".hammenu__close");
 
+const blur = document.querySelector(".blur");
+
 nav__menu.addEventListener("click", function () {
   hammenu.style.display = "block";
+  blur.style.display = "block";
 });
 
 hammenu__close.addEventListener("click", function () {
   hammenu.style.display = "none";
+  blur.style.display = "none";
 });
